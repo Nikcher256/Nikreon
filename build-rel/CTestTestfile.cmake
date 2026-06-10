1e14f06761be84f3da265f7e625e6c1d445606a3
Nikcher256 <nikitacherevan@gmail.com>
2026-06-10 18:47:32 +0200

    some

--- build-rel/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nikit/OneDrive/documents/Nikreon
-# Build directory: C:/Users/nikit/OneDrive/documents/Nikreon/build-rel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/NikreonUI")
